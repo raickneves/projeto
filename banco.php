954a16103488f3a38ab70c063aa2175a699abd9d
raickneves <129520616+raickneves@users.noreply.github.com>
2023-10-18 16:32:34 -0300

    w
    w

--- banco.php
+++ banco.php
@@ -1,7 +1,7 @@
 <?php
 $banco = new mysqli("localhost","root","","bd_games");
 if ($banco -> connect_errno){
-    echo" encontrei um erro $banco--> errno --> $banco-> connect_erro";
+    echo" encontrei um erro $banco-> errno --> $banco-> connect_erro";
     die();
 }
 
